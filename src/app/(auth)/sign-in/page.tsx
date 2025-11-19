dc42de94245c1acef3d3a8295c2f3bc3755b63ef
recurx-main <contact@recurx.xyz>
2025-11-19 23:59:19 +0530

    first commit

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import Link from "next/link";
+
 import Image from "next/image";
 
 import { authClient } from "@/lib/auth-client";
@@ -30,9 +30,7 @@ const SignIn = () => {
          
 
           
-           
-            
-         
+          
         </section>
 
         {/* Right Section - Sign In Card */}
